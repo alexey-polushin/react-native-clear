f6483aabf7aeed6cc8e21d0e5f45dc3c06df8c69
Unknown <leksi4ek@gmail.com>
2018-07-25 23:12:53 +0300

    react-native+redux+navigation

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Main extends Component {
+  static navigationOptions = null
+
+  render() {
+    return (
+      <View>
+        <Text>React Native</Text>
+      </View>
+    );
+  }
+}
+
+export default Main;
