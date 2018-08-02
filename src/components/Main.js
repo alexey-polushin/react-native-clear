006774d83c4dc27e437f6ad726e3657554d64d19
Unknown <leksi4ek@gmail.com>
2018-08-02 10:15:38 +0300

    add sigin

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Body, Title } from 'native-base';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
-class Main extends Component {
+export default class FormExample extends Component {
   static navigationOptions = {
     header: null,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      error: null,
+    };
+  }
+
+  signIn = async () => {
+    try {
+      const user = await GoogleSignin.signIn();
+      this.setState({ user, error: null });
+    } catch (error) {
+      if (error.code === 'CANCELED') {
+        error.message = 'user canceled the login flow';
+      }
+      this.setState({
+        error,
+      });
+    }
+  };
+
   render() {
     return (
-      <View>
-        <Text>React Native</Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Login</Title>
+          </Body>
+        </Header>
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
+          <GoogleSigninButton
+            style={{ width: 48, height: 48 }}
+            size={GoogleSigninButton.Size.Icon}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={this.signIn}
+          />
+        </Content>
+      </Container>
     );
   }
 }
-
-export default Main;
